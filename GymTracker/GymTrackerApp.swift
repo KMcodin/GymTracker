2c9f7572ba61eb0ca959de74d55e9bcfcf8ce183
Kevin Muina <muinakevin@gmail.com>
2021-07-16 10:16:18 -0400

    Initial commit

---
+++ GymTracker/GymTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackerApp.swift
+//  GymTracker
+//
+//  Created by Kevin Muina on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct GymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
